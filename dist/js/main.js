a65a44664b0b34c9e8c327cdd53fb8d9c11dc833
Nada Zeini <nada.elzeini@sjsu.edu>
2019-10-11 01:59:25 -0700

    burger icon done

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+//selecting dom items
+//want menu button
+//menu overlay
+const menuBtn = document.querySelector(".menu-button");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBrand = document.querySelector(".menu-branding");
+//more than one cant use query selector
+const navItems = document.querySelectorAll(".nav-item");
+
+//initial state of menu
+let showMenu = false;
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBrand.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBrand.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    showMenu = false;
+  }
+}
