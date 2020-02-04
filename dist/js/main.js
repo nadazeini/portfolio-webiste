b451c1119545dd6199e8dc5576605ba2798ec0f2
Nada Zeini <nada.elzeini@sjsu.edu>
2020-02-03 21:29:10 -0800

    repeat animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,35 +1,25 @@
-$(document).ready(function() {
-  $(".menu-button").on("click", function() {
+$(document).ready(function () {
+  $(".menu-button").on("click", function () {
     $(this).toggleClass("close");
     $(".menu").toggleClass("close");
   });
 
-  $(".menu .menu-nav").on("click", function() {
+  $(".menu .menu-nav").on("click", function () {
     $(".menu").removeClass("close");
     $(".menu-button").removeClass("close");
     $(".nav-list").removeClass("close");
   });
 
-  $('nav a[href *= "#"]').on("click", function() {
-    $("html,body").animate(
-      {
-        scrollTop: $($(this).attr("href")).offset().top - 100
-      },
-      2000
-    );
+  $('nav a[href *= "#"]').on("click", function () {
+    $("html,body").animate({
+      scrollTop: $($(this).attr("href")).offset().top - 100
+    }, 2000);
   });
-  $("#up").on("click", function() {
-    $("html,body").animate(
-      {
-        scrollTop: 0
-      },
-      2000
-    );
+  $("#up").on("click", function () {
+    $("html,body").animate({
+      scrollTop: 0
+    }, 2000);
   });
 
-  AOS.init({
-    easing: "ease",
-    duration: 2000,
-    once: true
-  });
+  AOS.init({easing: "ease", duration: 2000});
 });
